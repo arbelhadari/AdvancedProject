b66faa92f13ca1a4e84bfa2720c8181ceed84d61
arbelhadari <arbelhadari@gmail.com>
2023-04-18 18:26:05 +0300

    course.js changes bla bla bla

--- router/course.js
+++ router/course.js
@@ -1,29 +1,29 @@
 const Course = require("../schemas/course");
+const Student = require('../schemas/student');
 const router = require("express").Router();
 const bcrypt = require("bcrypt");
 
 
-router.post('/creatCourse', async (req, res) => {
+router.post('/MyCourses/creatCourse', async (req, res) => {
 
-    const cours = await new Course({
+    const course = await new Course({
         CoursesName: req.body.CoursesName,
         Coursesid: req.body.Coursesid,
         ProfessorUserName: req.body.ProfessorUserName,
         Year: req.body.Year,
         Semester: req.body.Semester,
-        studentAndGrade: req.body.studentAndGrade,
         courseDetails: req.body.courseDetails
     })
     try {
-        await cours.save();
-        res.status(200).json(cours)
+        await course.save();
+        res.status(200).json(course)
     } catch (err) {
         console.log(err);
         res.status(500).json(err)
     }
 })
 
-router.delete("/deleteCourse", async (req, res) => {
+router.delete("/MyCourses/deleteCourse", async (req, res) => {
     try {
         Course.findOneAndDelete({ Coursesid: req.body.Coursesid }, async (err, data) => {
             if (err) {
@@ -38,7 +38,64 @@ router.delete("/deleteCourse", async (req, res) => {
     }
 });
 
-router.put("/updateGradeStudent", async (req, res) => {
+router.post("/Course/addStudent", async (req, res) => {
+
+    if (!req.body.Coursesid || !req.body.ProfessorUserName)
+        return res.status(403).json("Missing courseid or username."); 
+
+    try {
+        Course.find({ Coursesid: req.body.Coursesid }, async (course_err, course) => {
+            if (!course_err) {
+                Student.find({studentId: req.body.studentId}), async (student_err, student) => {
+                    if (student_err) {
+                        const new_student = new Student({
+                            StudentId: req.body.StudentId,
+                            StudentDOB: req.body.StudentDOB,
+                            Gender: req.body.Gender
+                        })
+                        try {
+                            await new_student.save();
+                            res.status(200).json(new_student)
+                        } catch (err) {
+                            console.log(err);
+                            res.status(500).json(err)
+                        }
+                        course[0].GradesSheet.set(req.body.StudentId, req.body.Grade)
+                        try {
+                            await course[0].save();
+                            res.status(200).json(course[0]);
+                        } catch (err) {
+                            console.log(err);
+                            res.status(500).json(err);
+                        }
+                    }
+                    else {
+                        if (course[0].GradesSheet.has(student[0].StudentId)) {
+                            res.status(400).send({ error: "Sdudent ID already exists in this course."})
+                        }
+                        else {
+                            course[0].GradesSheet.set(req.body.StudentId, req.body.Grade)
+                            try {
+                                await course[0].save();
+                                res.status(200).json(course[0]);
+                            } catch (err) {
+                                console.log(err);
+                                res.status(500).json(err);
+                            }
+                        }
+                    }
+                }
+            }
+            else {
+                res.status(500).json(err)
+            }
+        })
+    } catch (err) {
+        res.status(501).json(err)
+    }
+});
+
+router.put("/Course/updateStudent", async (req, res) => {
     if (!req.body.Coursesid || !req.body.lecturersId)
         return res.status(403).json("err");
     try {
@@ -108,37 +165,8 @@ router.post("/deleteStudentAndGrade", async (req, res) => {
         res.status(501).json(err)
     }
 });
-//יצירת עדכון ציון סטודנט 
-router.post("/addStudentAndGrade", async (req, res) => {
 
-    if (!req.body.Coursesid || !req.body.lecturersId)
-        return res.status(403).json("err");
 
-    try {
-        Course.find({ Coursesid: req.body.Coursesid }, async (err, data) => {
-            if (err) res.status(500).json(err)
-            else {
-
-                let flag = false
-                let lecturersIds_ = data[0].lecturersIds;
-                lecturersIds_.forEach(element => {
-                    if (req.body.lecturersId == element)
-                        flag = true;
-                });
-                if (flag) {
-                    Course.findOneAndUpdate(
-                        { Coursesid: req.body.Coursesid }
-                        , { $push: { studentAndGrade: req.body.studentAndGrade } }, (err, data) => {
-                            if (err) res.status(500).json(err)
-                            else res.status(200).json(data)
-                        })
-                }
-            }
-        })
-    } catch (err) {
-        res.status(501).json(err)
-    }
-});
 
 router.post("/getCourse", async (req, res) => {
     if ((!req.body.lecturersId || !req.body.Coursesid))
